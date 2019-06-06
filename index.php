d7b658cce652888cf3c4b46bc5584672fb0ee47f
3053561 <3053561@IS-123904.ads.qub.ac.uk>
2019-06-06 15:36:38 +0100

    Second commit - bug fix that prevents users breaking the survey by continuously tapping the faces.

--- index.php
+++ index.php
@@ -13,15 +13,119 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+
+        <!--        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />-->
+
+
+        <link href="css/theme.css" rel="stylesheet" type="text/css"/>
+        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+
         <link rel="stylesheet" href="https://gitcdn.link/repo/Chalarangelo/mini.css/master/dist/mini-default.min.css">
         <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v2.3.4/dist/mini-default.min.css">
         <link href="css/styles.css" rel="stylesheet" type="text/css"/>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-touch-events/1.0.5/jquery.mobile-events.js"></script>
+
         <title>QUB Survey</title>
+        <style>
+
+            .ui-page { 
+                background-image:url('images/background.jpg');
+                background-size:     cover;                      
+                background-repeat:   no-repeat;        
+
+            }    
+
+        </style>
 
 
         <script>
+
+            function triggerCustomEvent(obj, eventType, event, touchData) {
+                var originalType = event.type;
+                event.type = eventType;
+
+                $.event.dispatch.call(obj, event, touchData);
+                event.type = originalType;
+            }
+
+            $.attrFn = $.attrFn || {};
+
+            // navigator.userAgent.toLowerCase() isn't reliable for Chrome installs
+            // on mobile devices. As such, we will create a boolean isChromeDesktop
+            // The reason that we need to do this is because Chrome annoyingly
+            // purports support for touch events even if the underlying hardware
+            // does not!
+            var agent = navigator.userAgent.toLowerCase(),
+                    isChromeDesktop = (agent.indexOf('chrome') > -1 && ((agent.indexOf('windows') > -1) || (agent.indexOf('macintosh') > -1) || (agent.indexOf('linux') > -1)) && agent.indexOf('mobile') < 0 && agent.indexOf('android') < 0), // Add Event shortcuts:
+                    settings = {
+                        tap_pixel_range: 5,
+                        swipe_h_threshold: 50,
+                        swipe_v_threshold: 50,
+                        taphold_threshold: 750,
+                        doubletap_int: 500,
+
+                        touch_capable: ('ontouchstart' in window && !isChromeDesktop),
+                        orientation_support: ('orientation' in window && 'onorientationchange' in window),
+
+                        startevent: (('ontouchstart' in window && !isChromeDesktop) ? 'touchstart' : 'mousedown'),
+                        endevent: (('ontouchstart' in window && !isChromeDesktop) ? 'touchend' : 'mouseup'),
+                        moveevent: (('ontouchstart' in window && !isChromeDesktop) ? 'touchmove' : 'mousemove'),
+                        tapevent: ('ontouchstart' in window && !isChromeDesktop) ? 'tap' : 'click',
+                        scrollevent: ('ontouchstart' in window && !isChromeDesktop) ? 'touchmove' : 'scroll',
+
+                        hold_timer: null,
+                        tap_timer: null
+                    };
+
+            $.each(['tapstart'], function (i, name) {
+                $.fn[name] = function (fn) {
+                    return fn ? this.on(name, fn) : this.trigger(name);
+                };
+
+                $.attrFn[name] = true;
+            });
+
+            // tapstart Event:
+            $.event.special.tapstart = {
+                setup: function () {
+
+                    var thisObject = this,
+                            $this = $(thisObject);
+
+                    $this.on(settings.startevent, function tapStartFunc(e) {
+
+                        $this.data('callee', tapStartFunc);
+                        if (e.which && e.which !== 1) {
+                            return false;
+                        }
+
+                        var origEvent = e.originalEvent,
+                                touchData = {
+                                    'position': {
+                                        'x': ((settings.touch_capable) ? origEvent.touches[0].screenX : e.screenX),
+                                        'y': (settings.touch_capable) ? origEvent.touches[0].screenY : e.screenY
+                                    },
+                                    'offset': {
+                                        'x': (settings.touch_capable) ? Math.round(origEvent.changedTouches[0].pageX - $this.offset().left) : Math.round(e.pageX - $this.offset().left),
+                                        'y': (settings.touch_capable) ? Math.round(origEvent.changedTouches[0].pageY - $this.offset().top) : Math.round(e.pageY - $this.offset().top)
+                                    },
+                                    'time': Date.now(),
+                                    'target': e.target
+                                };
+
+                        triggerCustomEvent(thisObject, 'tapstart', e, touchData);
+                        return true;
+                    });
+                },
+
+                remove: function () {
+                    $(this).off(settings.startevent, $(this).data.callee);
+                }
+            };
 
             $(document).ready(function () {
                 $("#thankyou").hide();
@@ -33,8 +137,16 @@ and open the template in the editor.
                 $("form").hide();
 
                 $("#smile").on('tapstart', function (e) {
-                    
-                    $("#smile").prop("disabled", "disabled");
+
+                    $('#smile').prop('disabled', true);
+                    setTimeout(function () {
+                        $('#smile').prop('disabled', false);
+                    }, 2350);
+
+                    $('#sad').prop('disabled', true);
+                    setTimeout(function () {
+                        $('#sad').prop('disabled', false);
+                    }, 2350);
 
                     e.preventDefault();
 
@@ -64,7 +176,18 @@ and open the template in the editor.
                 });
 
                 $("#sad").on('tapstart', function (e) {
-                    
+
+                    $('#smile').prop('disabled', true);
+                    setTimeout(function () {
+                        $('#smile').prop('disabled', false);
+                    }, 2350);
+
+                    $('#sad').prop('disabled', true);
+                    setTimeout(function () {
+                        $('#sad').prop('disabled', false);
+                    }, 2350);
+
+
                     e.preventDefault();
 
                     $("#sad").fadeOut(500);
@@ -88,7 +211,7 @@ and open the template in the editor.
                         }
 
                     });
-                    
+
                     $("form").hide().delay(500).fadeIn(500);
 
                     var timer = setTimeout(function () {
@@ -110,12 +233,14 @@ and open the template in the editor.
                                 console.log(data);
 
                                 var error = "Error";
-                                
+
 
                                 if (data === error) {
-  
                                     
+                                
                                     
+                                    var timer 
+                    
                                     console.log("here");
                                     $("form").fadeOut(100);
 
@@ -124,11 +249,8 @@ and open the template in the editor.
                                     $("#feedback2").hide().delay(100).fadeIn(500);
                                     $("#feedback2").hide().delay(600).fadeOut(500);
                                     $("form").hide().delay(1500).fadeIn(500);
-                                    $("#studentin").val(''); 
-                                  
-                                     var timer = setTimeout(function () {
-                        window.location = 'index.php';
-                    }, 17000);
+                                    $("#studentin").val('');
+
                                 } else {
 
                                     $("form").fadeOut(100);
@@ -143,8 +265,8 @@ and open the template in the editor.
                                     $("#studentin").val('');
 
                                     clearTimeout(timer);
-                                } 
-                              
+                                }
+
                             }});
 
                     });
@@ -179,7 +301,7 @@ and open the template in the editor.
         <div class="row">   
 
             <div class="col-sm-6">  
-                <button id="smile"><input type='hidden' name='smile'><img src="images/smile.png"  class="center left"/></button>
+                <button id="smile" ><input type='hidden' name='smile'><img src="images/smile.png"  class="center left"/></button>
             </div>  
             <div class="col-sm-6">
                 <button id="sad"><input type='hidden' name='sad'><img src="images/sad.png"  class="center right" width="200px"/></button>  
@@ -252,6 +374,8 @@ and open the template in the editor.
                 </div>
             </div>
         </form>
+
+
 
     </body>
 </html>
